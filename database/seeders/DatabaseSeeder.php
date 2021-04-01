8d263492ea920ac634afd4e52282488fa0f80460
AJninja <70128623+AJninja@users.noreply.github.com>
2021-04-01 16:51:07 +0530

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(class: PersonTableSeeder::class);
+        $this->call (class: PersonTableSeeder::class);
     }
 }
