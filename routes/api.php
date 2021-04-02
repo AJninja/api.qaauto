e8b2cc6e096125a615558efa7051fd9523af278f
AJninja <70128623+AJninja@users.noreply.github.com>
2021-04-02 16:44:04 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('/person', 'PersonController');
+//Route::apiResource('/person', 'API\v1\PersonController');
+
+Route::prefix('v1')->group(function(){
+    Route::apiResource('/person', 'Api\v1\PersonController')
+        ->only(['show']);
+
+});
